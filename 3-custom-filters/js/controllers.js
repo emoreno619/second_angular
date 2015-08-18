e1c3c90e766267e12e74527af76b9153828959ed
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-18 15:51:18 -0700

    adds first http request

--- 3-custom-filters/js/controllers.js
+++ 3-custom-filters/js/controllers.js
@@ -3,7 +3,11 @@ app.controller("HomeController", function($scope){
 	$scope.home = 1
 });
 
-app.controller("DogsController", function($scope){
+app.controller("DogsController", function($scope, $http){
 	$scope.dogs = "many_dogs_go_to_heaven"
 	$scope.cats = "though-many-cats-don't"
+
+	$http.get('https://api.github.com/zen').then(function(data){
+	    $scope.zenData = data.data;
+	});
 });
