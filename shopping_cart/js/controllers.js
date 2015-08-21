728cb3b70212e22e53b0a1eb22bdfb75b1993b45
Eduardo Moreno <emoreno@uchicago.edu>
2015-08-21 15:52:05 -0700

    formats prices

--- shopping_cart/js/controllers.js
+++ shopping_cart/js/controllers.js
@@ -1,3 +1,17 @@
 app.controller("homeController", ["$scope", "Data" , function($scope, Data){
 	$scope.teas = Data.teas
+
+	Data.teas.forEach(function(aTea){
+		var aPrice = aTea.price.toString()
+		var cents = aPrice.substring(2)
+		
+		aPrice = '$' + aPrice.substring(0,aPrice.length -2) + '.' + cents
+		aTea.price = aPrice
+		console.log(aTea.price)
+	})
+
+
+
+	$scope.cart = {}
+	$scope.cart.quantity = "Empty!"
 }])
